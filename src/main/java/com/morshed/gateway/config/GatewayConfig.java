0ce6200da19f652c6bbc83d2fb77b3430ece5bcf
Morshedul Basher <morshedb@hotmail.com>
2020-05-15 15:12:20 -0500

    remove swagger documentation

--- src/main/java/com/morshed/gateway/config/GatewayConfig.java
+++ src/main/java/com/morshed/gateway/config/GatewayConfig.java
@@ -19,11 +19,11 @@ public class GatewayConfig {
                 .route(P -> P
                         .path("/api/users")
                         .filters(f -> f.hystrix(config -> config.setName("api-test")
-                                        .setFallbackUri("forward:/apifailed")))
+                                        .setFallbackUri("forward:/api/apifailed")))
                         .uri("https://reqres.in/"))
                 .route(em -> em
                         .path("/api/v1/employees")
-                        .filters(emf -> emf.hystrix(config -> config.setName("employee-api").setFallbackUri("forward:/apifailed")))
+                        .filters(emf -> emf.hystrix(config -> config.setName("employee-api")))
                         .uri("http://dummy.restapiexample.com/"))
                 .route(p -> p
                         .host("*.hystrix.com")
@@ -31,5 +31,4 @@ public class GatewayConfig {
                         .uri("http://httpbin.org:80"))
                 .build();
     }
-
 }
